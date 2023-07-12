0a523e51545ca1a6b8cd8640179f4243f904b6ec
Jaydeep Chauhan <jaydeeplc@hotmail.com>
2023-07-12 10:07:27 +0530

    init

---
+++ HelloWorld.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract HelloWorld {
+    string public myString = "Hello World";
+    // Write your code here
+    string public greet = "Hello World";    
+
+}
